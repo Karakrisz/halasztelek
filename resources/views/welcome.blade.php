0258efb769a74c25f65ec7d41f807a624be78fa9
Boaz <webevolution13@gmail.com>
2023-10-03 14:20:13 +0200

    upDate
    front end own code

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,46 +30,74 @@
 
         <div class="about-content__img-text-box d-flex">
 
-            <div class="about-content__img-text-box__div">
+            <div class="about-content__img-text-box__div position-relative">
+                <div class="about-content__img-text-box__div__number-box position-absolute">
+                    <p class="about-content__img-text-box__div__number-box__p">-25%</p>
+                </div>
                 <div class="about-content__img-text-box__div__margin-box">
                     <img class="about-content__img-text-box__div__img" src="img/about/product.webp"
                         alt="gazdaesbadogos">
                     <div class="about-content__img-text-box__div__bottom-panel d-flex">
-                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
-                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--font-fomating">
+                            500 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--next-font-formating">
+                            325 Ft</p>
                     </div>
                 </div>
             </div>
 
-            <div class="about-content__img-text-box__div">
+            <div class="about-content__img-text-box__div position-relative">
+                <div class="about-content__img-text-box__div__number-box position-absolute">
+                    <p class="about-content__img-text-box__div__number-box__p">-25%</p>
+                </div>
                 <div class="about-content__img-text-box__div__margin-box">
                     <img class="about-content__img-text-box__div__img" src="img/about/product-2.webp"
                         alt="gazdaesbadogos">
                     <div class="about-content__img-text-box__div__bottom-panel d-flex">
-                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
-                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--font-fomating">
+                            500 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--next-font-formating">
+                            325 Ft</p>
                     </div>
                 </div>
             </div>
 
-            <div class="about-content__img-text-box__div">
+            <div class="about-content__img-text-box__div position-relative">
+                <div class="about-content__img-text-box__div__number-box position-absolute">
+                    <p class="about-content__img-text-box__div__number-box__p">-25%</p>
+                </div>
                 <div class="about-content__img-text-box__div__margin-box">
                     <img class="about-content__img-text-box__div__img" src="img/about/product-3.webp"
                         alt="gazdaesbadogos">
                     <div class="about-content__img-text-box__div__bottom-panel d-flex">
-                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
-                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--font-fomating">
+                            500 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--next-font-formating">
+                            325 Ft</p>
                     </div>
                 </div>
             </div>
 
-            <div class="about-content__img-text-box__div">
+            <div class="about-content__img-text-box__div position-relative">
+                <div class="about-content__img-text-box__div__number-box position-absolute">
+                    <p class="about-content__img-text-box__div__number-box__p">-25%</p>
+                </div>
                 <div class="about-content__img-text-box__div__margin-box">
                     <img class="about-content__img-text-box__div__img" src="img/about/product-4.webp"
                         alt="gazdaesbadogos">
                     <div class="about-content__img-text-box__div__bottom-panel d-flex">
-                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
-                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--font-fomating">
+                            500 Ft</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p 
+                            about-content__img-text-box__div__bottom-panel__p--next-font-formating">
+                            325 Ft</p>
                     </div>
                 </div>
             </div>
