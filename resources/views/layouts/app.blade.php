9dd0eecf4c9103a1f06483d6afb40e4d013146b1
Boaz <webevolution13@gmail.com>
2023-10-03 13:07:07 +0200

    upDate
    front end own code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,8 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;300;400;500;600;700;900&display=swap"
         rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=League+Spartan:wght@300;400;500;600;700;800;900&display=swap"
+        rel="stylesheet">
     <!-- SEO -->
     <meta property="og:title" content="gazdaesbadogosbolt" />
     <meta property="og:image" content="" />
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,87 @@
 @extends('layouts.app')
 
 @section('content')
+<section>
+    <div class="slider-content position-relative">
+        <img class="slider-content__img" src="img/slider/slider.webp" alt="gazdaesbadogos">
+
+        <div class="slider-content__img-text-box d-flex position-absolute">
+
+            <div class="slider-content__img-text-box__text-box t-end">
+                <h1 class="slider-content__img-text-box__text-box__h1 text-transform-uppercase">Lorem ipsum dolor sit
+                    amet
+                    consectetur iaculis eu
+                    vitae aenean</h1>
+            </div>
+
+            <div class="slider-content__img-text-box__img-box">
+                <img class="slider-content__img-text-box__img-box__img" src="img/slider/mdi_screw-flat-top.svg"
+                    alt="gazdaesbadogos">
+            </div>
+
+        </div>
+    </div>
+</section>
+
+<section>
+    <div class="about-content">
+
+        <h2 class="about-content__h2 text-transform-uppercase text-center">AKCIÓINK</h2>
+
+        <div class="about-content__img-text-box d-flex">
+
+            <div class="about-content__img-text-box__div">
+                <div class="about-content__img-text-box__div__margin-box">
+                    <img class="about-content__img-text-box__div__img" src="img/about/product.webp"
+                        alt="gazdaesbadogos">
+                    <div class="about-content__img-text-box__div__bottom-panel d-flex">
+                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="about-content__img-text-box__div">
+                <div class="about-content__img-text-box__div__margin-box">
+                    <img class="about-content__img-text-box__div__img" src="img/about/product-2.webp"
+                        alt="gazdaesbadogos">
+                    <div class="about-content__img-text-box__div__bottom-panel d-flex">
+                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="about-content__img-text-box__div">
+                <div class="about-content__img-text-box__div__margin-box">
+                    <img class="about-content__img-text-box__div__img" src="img/about/product-3.webp"
+                        alt="gazdaesbadogos">
+                    <div class="about-content__img-text-box__div__bottom-panel d-flex">
+                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="about-content__img-text-box__div">
+                <div class="about-content__img-text-box__div__margin-box">
+                    <img class="about-content__img-text-box__div__img" src="img/about/product-4.webp"
+                        alt="gazdaesbadogos">
+                    <div class="about-content__img-text-box__div__bottom-panel d-flex">
+                        <p class="about-content__img-text-box__div__bottom-panel__p">500</p>
+                        <p class="about-content__img-text-box__div__bottom-panel__p">325 Ft</p>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="about-content__link-box text-center">
+            <a class="page-link" href="#">Összes akció</a>
+        </div>
+
+    </div>
+</section>
 
 
 @endsection
